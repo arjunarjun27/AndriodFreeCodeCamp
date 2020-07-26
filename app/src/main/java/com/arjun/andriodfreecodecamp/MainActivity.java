3fb5319c2974cf7e515c11b9cca2e321c892ed34
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-27 01:35:12 +0530

    loops

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -11,9 +11,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      // loops
+        // loops
 
 
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+        int a = 5;
+
+        while (a < 10) {
+            a++;
+
+            if (a == 8) {
+                continue;
+            }
+
+            System.out.println("Hello");
+
+        }
+
+
+        do {
+            System.out.println("Hello");
+
+        } while (a >5);
 
     }
 }
