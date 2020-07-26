7c18f329bf20029f0e0e55e3c810d8bc93e8fc9f
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-26 20:13:34 +0530

    loops initail

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -11,23 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Arithmetic Operator +,-,*,/,%
-
-        int a =5;
-        a+=2;
-        int b=2;
-        b++; // 3
-        b--;//2
-
-        // relational or comparison operator <.<=,>,>=,==,!=
-
-        boolean answaer =a<b;
-
-
-        // logical operator ||,&&,
-
-        boolean ans=a==5 || b==2;
-
+      // loops
 
 
 
