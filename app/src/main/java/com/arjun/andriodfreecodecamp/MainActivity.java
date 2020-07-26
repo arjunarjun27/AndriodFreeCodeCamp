ac7a478f3e3887b3e5e2c7c5f086c94ed5a59866
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-27 03:35:18 +0530

    arrays

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -3,39 +3,55 @@ package com.arjun.andriodfreecodecamp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String enteredName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.editText);
+        Button submitButton = findViewById(R.id.button);
+        final TextView textView = findViewById(R.id.mytextview);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                enteredName = editText.getText().toString();
+                String[] names = new String[5];
+                names[1] = "Arjun";
+                names[2] = "Anand";
 
-        // loops
+
+                String[] mynames = {"Arjun", "anand", "mohan", "selva"};
+                int[] mark = {40, 50, 55, 87};
 
 
-        for (int i = 0; i < 10; i += 2) {
-            System.out.println(i);
-        }
+                for (int i = 0; i < mynames.length; i++) {
 
-        int a = 5;
+                    System.out.println(mynames[i]);
+                }
 
-        while (a < 10) {
-            a++;
 
-            if (a == 8) {
-                continue;
+                for (int i = 0; i < mynames.length; i++) {
+                    if (enteredName.equals(mynames[i])) {
+                        int m = mark[i];
+                        textView.setText(String.valueOf(m));
+                    }
+
+                }
             }
-
-            System.out.println("Hello");
-
-        }
+        });
 
 
-        do {
-            System.out.println("Hello");
 
-        } while (a >5);
+
 
     }
 }
