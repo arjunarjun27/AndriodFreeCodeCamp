d8a1c2acb8dd375a1ff5c17efbdcf051449520ea
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-26 19:40:10 +0530

    variables

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -11,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // variables
+
+        int a=-5;
+        double mydouble=2.5;
+
+
     }
 }
