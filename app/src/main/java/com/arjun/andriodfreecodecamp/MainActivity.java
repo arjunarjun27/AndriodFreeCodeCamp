871ae3ac18b11ab7b35d7399066dee619498ab7d
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-28 08:22:09 +0530

    lists

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -4,12 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ArrayList<String> names=new ArrayList<>();
+        List<String> mylist=new ArrayList<>();// using interfaces
+
+
+        names.add("Arjun");
+        names.add("Anand");
+        names.add("mohan");
+        names.add("Sachin");
+        names.get(0);
+
+        names.clear();
+        names.contains("Arjun");
+        names.remove("mohan");
+        names.indexOf("Arjun");
+
+
+
+        for(int i=0;i<names.size();i++){
+            System.out.println(names.get(i));
+        }
 
 
     }
