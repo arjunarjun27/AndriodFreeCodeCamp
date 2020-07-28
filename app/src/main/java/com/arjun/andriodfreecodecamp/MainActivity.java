45e3486a0499afcecb3aff5f634bf44ab5fc3503
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-28 18:35:37 +0530

    concurrncy/asynchronous

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -12,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // worker thread
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i=0;i<5;i++){
+                    System.out.println("Printing "+i+"in a worker thread");
+                    try {
+                        Thread.sleep(i);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+
+            }
+        });
+        thread.start();
+
+
+        // main thread
+        for (int i=0;i<5;i++){
+            System.out.println("Printing "+i+"in a Main thread");
+            try {
+                Thread.sleep(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 }
