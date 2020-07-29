a12e77b754ecc875a718169c4992bd26717cfd08
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-29 12:41:04 +0530

    Radiobutton

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -5,45 +5,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    EditText editText;
-    Button button;
-    TextView textView;
+public class MainActivity extends AppCompatActivity {
+
+
+    CheckBox checkBox;
+    RadioGroup rgMartialStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkBox = findViewById(R.id.checkBox1);
+        rgMartialStatus=findViewById(R.id.rgMartialstatus);
 
-        editText = findViewById(R.id.editText);
-        button = findViewById(R.id.btnHello);
-        button.setOnClickListener(this);
-        textView = findViewById(R.id.txtHello);
 
-        button.setOnLongClickListener(new View.OnLongClickListener() {
+        if (checkBox.isChecked()) {
+            Toast.makeText(MainActivity.this, "Checked", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(MainActivity.this, "Not Checked", Toast.LENGTH_SHORT).show();
+
+        }
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(MainActivity.this, "Long press", Toast.LENGTH_SHORT).show();
-                return true;
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (b) {
+                    Toast.makeText(MainActivity.this, "Checked", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Checked", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        rgMartialStatus.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.single:
+                        Toast.makeText(MainActivity.this, "Single", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.married:
+                        Toast.makeText(MainActivity.this, "Married", Toast.LENGTH_SHORT).show();
+
+                        break;
+                    case R.id.relationship:
+                        Toast.makeText(MainActivity.this, "Relationship", Toast.LENGTH_SHORT).show();
+
+                        break;
+                }
+
+
+
             }
         });
 
 
     }
 
-    @Override
-    public void onClick(View view) {
-
-        switch (view.getId()) {
-            case R.id.btnHello:
-                textView.setText("Hello " + editText.getText().toString());
-                break;
-
-        }
-
-    }
 }
