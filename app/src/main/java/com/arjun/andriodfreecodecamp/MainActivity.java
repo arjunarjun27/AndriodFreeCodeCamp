aba42e3ce1bc47267d97341efbd3222014ea7629
arjunarjun27 <arjunarjun27@gmail.com>
2020-07-31 18:59:45 +0530

    Listview

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -3,29 +3,52 @@ package com.arjun.andriodfreecodecamp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listView=findViewById(R.id.myListview);
+
+
+        final ArrayList<String> citesList=new ArrayList<>();
+        citesList.add("Cheenai");
+        citesList.add("Madurai");
+        citesList.add("Salem");
+        citesList.add("Coimbatore");
+
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(
+                this,android.R.layout.simple_list_item_1,citesList
+
+        );
 
 
 
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Toast.makeText(MainActivity.this, citesList.get(i)+" Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
