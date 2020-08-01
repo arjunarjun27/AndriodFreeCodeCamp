6d07825003538c02e2ceec891c7e96c2b2654593
arjunarjun27 <arjunarjun27@gmail.com>
2020-08-01 20:19:32 +0530

    master clean

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -17,33 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(getString(R.string.Hello));
 
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings_menu:
-                Toast.makeText(this, "setting selected", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.alarm_menu:
-                Toast.makeText(this, "alarm selected", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
 
     }
+
+
 }
