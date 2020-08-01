7e9d1c3e8a0b9dd4f7779d96e7a86090594f3bd2
arjunarjun27 <arjunarjun27@gmail.com>
2020-08-02 04:37:56 +0530

    snackbar

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -3,24 +3,33 @@ package com.arjun.andriodfreecodecamp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
 
+    Button button;
+    RelativeLayout parent;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        parent=findViewById(R.id.parent);
+        button=findViewById(R.id.button);
         fab=findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(parent,"This is a snackbar",Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Retry", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        })
+                        .setActionTextColor(Color.RED)
+                        .setTextColor(Color.YELLOW)
+                        .show();
+
+            }
+        });
 
     }
 
