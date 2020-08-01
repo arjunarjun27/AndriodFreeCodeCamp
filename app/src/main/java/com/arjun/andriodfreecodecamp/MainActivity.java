44e53d8e9a1219ced05bd21db178b29046c8f209
arjunarjun27 <arjunarjun27@gmail.com>
2020-08-01 19:45:35 +0530

    #menuresource #actionbar menu

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -1,9 +1,14 @@
 package com.arjun.andriodfreecodecamp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +17,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView=findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.textView);
         textView.setText(getString(R.string.Hello));
 
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_menu:
+                Toast.makeText(this, "setting selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.alarm_menu:
+                Toast.makeText(this, "alarm selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
 
     }
 }
