c946c5311aa3da7cbcc8c0c9525d48f610b78df3
arjunarjun27 <arjunarjun27@gmail.com>
2020-08-01 17:56:17 +0530

    spinner dynamic and static

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     ListView listView;
+    Spinner spinner;
+    Spinner staticSpiner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView=findViewById(R.id.myListview);
-
+        spinner=findViewById(R.id.mySpinner);
+        staticSpiner=findViewById(R.id.staticSpinner);
 
         final ArrayList<String> citesList=new ArrayList<>();
         citesList.add("Cheenai");
@@ -40,13 +44,52 @@ public class MainActivity extends AppCompatActivity {
 
 
         listView.setAdapter(adapter);
-
-
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
                 Toast.makeText(MainActivity.this, citesList.get(i)+" Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+        final ArrayList<String> spinnerdata=new ArrayList<>();
+        spinnerdata.add("Arjun");
+        spinnerdata.add("Murugan");
+        spinnerdata.add("Kumaran");
+
+
+        ArrayAdapter<String> spinnerAdapter=new ArrayAdapter<>(
+                this,android.R.layout.simple_spinner_item,spinnerdata
+
+        );
+
+        
+        spinner.setAdapter(spinnerAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, ""+spinnerdata.get(i)+" Selected", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        staticSpiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, staticSpiner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
 
