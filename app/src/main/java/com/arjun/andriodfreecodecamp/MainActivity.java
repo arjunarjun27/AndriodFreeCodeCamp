9b57348030c9982f71e4066a07e55aacce0edfc3
arjunarjun27 <arjunarjun27@gmail.com>
2020-08-02 23:34:41 +0530

    fonts

--- app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
+++ app/src/main/java/com/arjun/andriodfreecodecamp/MainActivity.java
@@ -2,11 +2,14 @@ package com.arjun.andriodfreecodecamp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,8 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button=findViewById(R.id.button);
+
+        // set font in programmimg
 
 
+        //Typeface typeface=getResources().getFont(R.font.lobster_two_bold); //requires  26
+        Typeface typeface= ResourcesCompat.getFont(this,R.font.lobster_two_bold);
+
+
+        button.setTypeface(typeface);
 
     }
 
